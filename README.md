dd84ac35f627fbc13253fe77b92710431a2e4fc8
LoveJuzi <jike080401@163.com>
2023-12-02 11:17:53 +0800

    README.md

--- README.md
+++ README.md
@@ -15,3 +15,25 @@ export LIBGL_ALWAYS_SOFTWARE=1
 export PYOPENGL_PLATFORM=x11
 ```
 
+## PyGLM
+
+通常建议使用 numpy 库，但是，为了学习的方便我们可以使用 PyGLM 库
+
+``` sh
+pip install PyGLM
+```
+
+## 阿里云镜像
+
+设置镜像方便下载库
+
+``` sh
+pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
+```
+
+使用如下指令用来恢复默认的镜像源
+
+``` sh
+pip config unset global.index-url
+```
+
---
+++ demo12_matrix/main.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import numpy as np
+import glm
+
+# 创建一个4x4的矩阵
+mat4 = np.array([
+    [1.0, 0.0, 0.0, 0.0],
+    [0.0, 1.0, 0.0, 0.0],
+    [0.0, 0.0, 1.0, 0.0],
+    [0.0, 0.0, 0.0, 1.0]
+])
+
+# 打印矩阵
+print(mat4)
+
+vec = glm.vec4(1.0, 0.0, 0.0, 1.0)
+print(vec)
+
+trans = glm.mat4()
+trans = glm.translate(trans, glm.vec3(1, 1, 0))
+print(trans)
+
+vec = trans * vec
+print(vec)
+
+glm.scale(glm.mat4(), glm.vec3(0.5, 0.5, 0.5))
+glm.rotate(glm.mat4(), glm.radians(90.0), glm.vec3(0, 0, 1))
