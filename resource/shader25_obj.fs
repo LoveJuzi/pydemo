f5c5c43c20a885be2526785dd3bce444cd7e940e
LoveJuzi <mylovejuzi@icloud.com>
2023-12-16 12:47:24 +0800

    demo25

--- resource/shader25_obj.fs
+++ resource/shader25_obj.fs
@@ -8,7 +8,8 @@ struct Material {
 struct Light {
     vec3 position;
     vec3 direction;
-    float cutoff;
+    float cutOff;
+    float outerCutOff;
 
     vec3 ambient;
     vec3 diffuse;
@@ -31,21 +32,21 @@ void main() {
     vec3 lightDir = normalize(light.position - FragPos);
 
     float theta = dot(lightDir, normalize(-light.direction));
+    float epsilon = light.cutOff - light.outerCutOff;
+    float indensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
 
-    if (theta > light.cutoff) {
-        vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
 
-        float diff = max(dot(norm, lightDir), 0.0);
-        vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
 
-        vec3 viewDir = normalize(viewPos - FragPos);
-        vec3 reflectDir = reflect(-lightDir, norm);
-        float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shiniess);
-        vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shiniess);
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
 
-        FragColor = vec4(ambient + diffuse + specular, 1.0);
-    } else {
-        FragColor = vec4(light.ambient * vec3(texture(material.diffuse, TexCoords)), 1.0);
-    }
+    diffuse *= indensity;
+    specular *= indensity;
 
+    FragColor = vec4(ambient + diffuse + specular, 1.0);
 }
