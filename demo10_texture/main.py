35d80da21802283ff242b9c330e49aa989997dda
LoveJuzi <mylovejuzi@icloud.com>
2024-11-13 22:16:34 +0800

    update

--- demo10_texture/main.py
+++ demo10_texture/main.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 import numpy as np
 from pygame.locals import DOUBLEBUF
--- runenv.sh
+++ runenv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd $(dirname $(realpath ${BASH_SOURCE}))
+#cd $(dirname $(realpath ${BASH_SOURCE}))
 export PYTHONPATH="$PWD/utils:$PWD/resource"
-cd - > /dev/null
+#cd - > /dev/null
