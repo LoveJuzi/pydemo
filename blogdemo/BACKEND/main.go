5fb094f9195ab1fe5a3f05b44734ed6171761d76
LoveJuzi <mylovejuzi@icloud.com>
2025-01-10 00:06:35 +0800

    构建后端

---
+++ blogdemo/BACKEND/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// 实例
+	r := gin.Default()
+	// 路由
+	// 路由方法有 GET, POST, PUT, PATCH, DELETE 和 OPTIONS
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, Geektutu")
+	})
+	// 动态路由
+	r.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+	// 获取Query参数
+	r.GET("/users", func(c *gin.Context) {
+		name := c.Query("name")                   // 获取 name 参数
+		role := c.DefaultQuery("role", "teacher") // 设置默认参数
+		c.String(http.StatusOK, "%s is a %s", name, role)
+	})
+	// 获取POST参数
+	r.POST("/form", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.DefaultPostForm("password", "0000")
+
+		c.JSON(http.StatusOK, gin.H{
+			"username": username,
+			"password": password,
+		})
+	})
+	// Map参数
+	r.POST("/post", func(c *gin.Context) {
+		ids := c.QueryMap("ids")
+		names := c.PostFormMap("names")
+
+		c.JSON(http.StatusOK, gin.H{
+			"ids":   ids,
+			"names": names,
+		})
+	})
+	// 重定向
+	r.GET("/redirect", func(c *gin.Context) {
+		// c.Redirect(http.StatusMovedPermanently, "/index")
+		c.Redirect(http.StatusMovedPermanently, "/")
+	})
+	r.GET("/gohome", func(c *gin.Context) {
+		c.Request.URL.Path = "/"
+		r.HandleContext(c)
+	})
+	// 启动服务
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
---
+++ blogdemo/BACKEND/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#export GIN_MODE=release # 使用 gin 的 release 模式
+#(go run main.go) &
+#sleep 2
+
+echo "开始测试"
+
+echo "测试Map参数"
+curl -g "http://localhost:8080/post?ids[Jack]=001&ids[Tom]=002" -X POST -d 'names[a]=Sam&names[b]=David'
+echo
+
+echo "测试重定向"
+curl -i "http://localhost:8080/redirect"
+echo
+
+echo "测试重定向2"
+curl -i "http://localhost:8080/gohome"
+echo
